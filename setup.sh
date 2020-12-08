73d32c0de0d16770b6d8024a37ccbe3be94f5cf7
Gabe The Coder <gabethecoder@icloud.com>
2020-12-08 13:40:06 -0500

    updates

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 sh /media/usb2/apt-prefetch.sh
 
+apt-get install nvidia-driver-455 # can use prefetch bundle
+apt-get install powertop # can use prefetch bundle
+
 sudo systemctl stop snapd.service
 sudo systemctl mask snapd.service
 
 sh /media/usb2/workflow/sw-updates-never.sh
+sh /media/usb2/workflow/privacy-restrict.sh
+sh /media/usb2/workflow/disable-search.sh
+sh /media/usb2/workflow/disable-search-locations.sh
 sudo apt --fix-broken install
+
+apt-get install gnome-tweak-tool # can use prefetch bundle
